22df950560ab83e5693c0f29ae515b40ad85a616
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-25 20:26:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+var n = prompt("Enter a string");
+let map={}
+let flag=false;
+n.split().forEach(val=>{
+if(map[val]){
+	map.val=map.val+1;
+}else{
+	map.val=1
+}
+})
+n.split('').forEach(val=>{if(map.val===1&&!flag){alert(val);flag=true }})
+
